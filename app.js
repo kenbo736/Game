5e2c0dc64fd6efa84049cc2059da5d7b30dc7db4
Kenneth Börjesson <kenbo736@student.liu.se>
2016-10-18 13:36:53 +0200

    adding chatbox

--- app.js
+++ app.js
@@ -159,7 +159,18 @@ io.sockets.on('connection', function(socket){
         delete SOCKET_LIST[socket.id];
         Player.onDisconnect(socket);
     });
-
+    socket.on('sendMsgToServer', function(data) {
+        var playerName = ("" + socket.id).slice(2, 7);
+        for(var i in SOCKET_LIST) {
+            SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data);
+        }
+    });
+    socket.on('evalServer', function(data) {
+        if(!DEBUG)
+            return;
+        var res = eval(data);
+        socket.emit('evalAnswer', res);
+    });
 });
 
 setInterval(function() {
